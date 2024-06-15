c6fd7e59918773596cb8926cb47684ba780313d8
unknown <tfaiza550@gmail.com>
2024-06-15 12:52:57 -0700

    added

--- src/main/java/com/example/assignment2/controllers/RectangleController.java
+++ src/main/java/com/example/assignment2/controllers/RectangleController.java
@@ -1,20 +1,23 @@
 package com.example.assignment2.controllers;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.assignment2.models.Rectangle;
+import com.example.assignment2.models.RectangleRepository;
 
 @Controller
 public class RectangleController {
+    @Autowired
+    private RectangleRepository rectangleRepo;
     @GetMapping("rectangles/view")
     public String getAllRectangles(Model model){
-        List<Rectangle> rectangles= new ArrayList<>();
-        rectangles.add(new Rectangle("bobby",10,10,"#ff0000"));
+        List<Rectangle> rectangles= rectangleRepo.findAll();
         model.addAttribute("rectangles", rectangles);
         return "rectangles/showAll";
     }
