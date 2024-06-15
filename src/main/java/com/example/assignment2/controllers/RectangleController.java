95546757ab73b6fb4bb92751a00a7548024741bb
unknown <tfaiza550@gmail.com>
2024-06-15 15:51:28 -0700

    added

--- src/main/java/com/example/assignment2/controllers/RectangleController.java
+++ src/main/java/com/example/assignment2/controllers/RectangleController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -41,5 +42,10 @@ public class RectangleController {
         return "redirect:/add.html";
     }
     
+    @GetMapping("/rectangles/delete/{id}")
+    public String deleteReactangle(@PathVariable int id){
+        rectangleRepo.deleteById(id);
+        return "redirect:/rectangles/view";
+    }
     
 }
